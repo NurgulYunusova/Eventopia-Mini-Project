7caa7dbb4d8d754ab1404a452aaa39bcb7323d4f
Nurgul Yunusova <nurgulyunusova03@gmail.com>
2023-06-11 17:32:36 +0400

    updated some parts

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,12 +2,10 @@ import "./header.scss";
 import { Link } from "react-router-dom";
 
 function Header() {
-  
   return (
     <>
       <header className="header">
         <div className="container">
-
           <h1 className="logo">
             <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
               Eventopia
@@ -15,9 +13,12 @@ function Header() {
           </h1>
 
           <div className="search-bar">
-            <input type="text" placeholder="Search for events, artists or venues..."/>
+            <input
+              type="text"
+              placeholder="Search for events, artists or venues..."
+            />
             <a href="#">
-              <i className='fas fa-search '></i>
+              <i className="fas fa-search "></i>
             </a>
           </div>
         </div>
